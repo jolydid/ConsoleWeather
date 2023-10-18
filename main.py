04756518079e921e040f3f518853ad6874729e38
didus <108202955+jolydid@users.noreply.github.com>
2023-10-18 14:44:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,19 +2,13 @@ import requests
 
 PLACES = ['London', 'SVO', 'Череповец']
 
-<<<<<<< HEAD
 def get_weather(url):   
     response = requests.get(url=url)
-    print(response.raise_for_status())
-=======
-def get_weather(url):
-    response = requests.get(url=url)
->>>>>>> a4654831d596ab6d06c5f768ff1234782dd8f463
+    response.raise_for_status()
     print(response.text)
 
 def main():
     for place in PLACES:
-<<<<<<< HEAD
         try:
             get_weather(f'https://wttr.in/{place}?nmMTqu&lang=ru')
         except Exception as e:
@@ -24,10 +18,4 @@ def main():
 
 if __name__ == '__main__':
     main()
-=======
-        get_weather(f'https://wttr.in/{place}?nmMTqu&lang=ru')
-    
-if __name__=='__main__':
-    main()
-    
->>>>>>> a4654831d596ab6d06c5f768ff1234782dd8f463
+
